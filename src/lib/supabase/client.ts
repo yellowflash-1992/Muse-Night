12837ca57ab90b459fac45c1cf05b984a5065bdd
yellowflash-1992 <abdulnasirmuhammad989@gmail.com>
2026-09-26 14:30:35 +0100

    add Supabase client setup

---
+++ src/lib/supabase/client.ts
@@ -0,0 +1,10 @@
+/// <reference types="vite/client" />
+
+import { createBrowserClient } from "@supabase/ssr";
+
+export function createClient() {
+  return createBrowserClient(
+    import.meta.env["VITE_SUPABASE_URL"]!,
+    import.meta.env["VITE_SUPABASE_PUBLISHABLE_KEY"]!,
+  );
+}
---
+++ src/lib/supabase/server.ts
@@ -0,0 +1,29 @@
+import { createServerClient } from "@supabase/ssr";
+import { getCookies, setCookie, setResponseHeader } from "@tanstack/react-start/server";
+
+export function createClient() {
+  return createServerClient(
+    process.env["VITE_SUPABASE_URL"]!,
+    process.env["VITE_SUPABASE_PUBLISHABLE_KEY"]!,
+    {
+      cookies: {
+        getAll() {
+          return Object.entries(getCookies()).map(([name, value]) => ({
+            name,
+            value,
+          }));
+        },
+
+        setAll(cookies, headers) {
+          cookies.forEach(({ name, value, options }) => {
+            setCookie(name, value, options);
+          });
+
+          Object.entries(headers).forEach(([name, value]) => {
+            setResponseHeader(name, value);
+          });
+        },
+      },
+    },
+  );
+}
